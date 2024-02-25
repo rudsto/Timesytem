74c8c979669f545cd6b90a3dcacd7818fc751b91
Severin <669799@stud.hvl.no>
2024-02-25 10:36:45 +0100

    init

---
+++ src/main/java/no/hvl/dat109/prosjekt/ProsjektGruppe7Application.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.prosjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsjektGruppe7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsjektGruppe7Application.class, args);
+	}
+
+}
---
+++ src/test/java/no/hvl/dat109/prosjekt/ProsjektGruppe7ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.prosjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsjektGruppe7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
