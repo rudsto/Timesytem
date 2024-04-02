749a99ff479fd3ac2309c8174250b350e03a5ac2
Student HVL Dataingeniør <111868697+667029@users.noreply.github.com>
2024-04-02 14:00:25 +0200

    Lagt til testklasser for Serviceklassene. TODO

---
+++ src/test/java/no/hvl/dat109/prosjekt/LoginServiceTest.java
@@ -0,0 +1,16 @@
+package no.hvl.dat109.prosjekt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoginServiceTest {
+	
+	//TODO
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
