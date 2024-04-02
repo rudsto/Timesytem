72f7b1c359eb63493a0f0141f1a49e0d768c34f3
Student HVL Dataingeniør <111868697+667029@users.noreply.github.com>
2024-04-02 13:45:53 +0200

    Opprettet BrukerService, med TODO metoder

---
+++ src/main/java/no/hvl/dat109/prosjekt/service/BrukerService.java
@@ -0,0 +1,29 @@
+package no.hvl.dat109.prosjekt.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import no.hvl.dat109.prosjekt.entity.Bruker;
+
+@Service
+public class BrukerService {
+	
+	//TODO
+	
+	public List<Bruker> finnAlle() {
+		//TODO
+		return null;
+	}
+	
+	public Bruker finnMedMobil(String mobil) {
+		//TODO
+		return null;
+	}
+	
+	public Bruker lagre(Bruker bruker) {
+		//TODO
+		return null;
+	}
+
+}
