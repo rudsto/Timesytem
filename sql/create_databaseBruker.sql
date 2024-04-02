b099b1f4cb1a1900ecd82614ebe3b406fbb1339f
Student HVL Dataingeniør <111868697+667029@users.noreply.github.com>
2024-04-02 12:55:17 +0200

    lagt til kode for sql-database

--- sql/create_databaseBruker.sql
+++ sql/create_databaseBruker.sql
@@ -5,8 +5,10 @@ SET search_path TO DAT109_PROSJEKT;
 CREATE TABLE bruker
 (
     mobil     CHARACTER(8) PRIMARY KEY,
-    fornavn   CHARACTER VARYING(40),
-    etternavn CHARACTER VARYING(40)
     hash      CHARACTER(64) NOT NULL,
     salt      CHARACTER(32) NOT NULL,
+    fornavn   CHARACTER VARYING(40),
+    etternavn CHARACTER VARYING(40)
 );
+
+select * from dat109_prosjekt.bruker;
