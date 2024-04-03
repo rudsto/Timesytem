c7b58335f60c1724185bb331c1492e3d1b2cb7f4
Severin <669799@stud.hvl.no>
2024-04-03 13:58:44 +0200

    javadoc looking good

--- src/main/java/no/hvl/dat109/prosjekt/service/LoginService.java
+++ src/main/java/no/hvl/dat109/prosjekt/service/LoginService.java
@@ -6,16 +6,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import no.hvl.dat109.prosjekt.entity.Bruker;
 
+/**
+ * Serviceklasse for login.
+ */
 @Service
 public class LoginService {
 
 	private static final int MAX_INTERACTIVE_INTERVAL = 10 * 60;
 
-
+	/**
+	 * Logger ut en bruker.
+	 * @param session
+	 */
 	public static void loggUtBruker(HttpSession session) {
 		session.invalidate();
 	}
 
+	/**
+	 * Logger inn en bruker.
+	 * @param request
+	 * @param bruker
+	 */
 	public static void loggInnBruker(HttpServletRequest request, Bruker bruker) {
 		loggUtBruker(request.getSession());
 		HttpSession session = request.getSession();
@@ -23,6 +34,11 @@ public class LoginService {
 		session.setAttribute("bruker", bruker);
 	}
 
+	/**
+	 * Sjekker om en bruker er innlogget.
+	 * @param session
+	 * @return
+	 */
 	public static boolean erBrukerInnlogget(HttpSession session) {
 		return session != null && session.getAttribute("bruker") != null;
 	}
