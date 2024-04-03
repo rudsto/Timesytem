3716deb166a37c1974b83bbf56a539983cc8e099
IverTM <ivertmalme@gmail.com>
2024-04-03 12:27:16 +0200

    projekt entity

--- src/main/java/no/hvl/dat109/prosjekt/entity/Prosjekt.java
+++ src/main/java/no/hvl/dat109/prosjekt/entity/Prosjekt.java
@@ -3,20 +3,26 @@ package no.hvl.dat109.prosjekt.entity;
 import java.util.List;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "prosjekt", schema = "dat109_prosjekt")
+
 public class Prosjekt {
 	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotNull(message = "Prosjektnavn er obligatorisk")
+    @OneToMany(mappedBy = "projekt", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Time> timeliste;
+
+
+    @Column(name = "Projektnavn", length = 50)
+    @Size(min = 1, max = 50, message = "Projektnavnet må være mellom 1-50 bokstaver")
+    @NotNull(message = "Projektet må ha navn")
     private String navn;
 
-    @OneToMany(mappedBy = "prosjekt")
-    private List<Time> timeliste;
 
     public Integer getId() {
         return id;
