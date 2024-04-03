6f3db08cf217fab427652b8e671ce9d5588e4c50
669802 <111427312+669802@users.noreply.github.com>
2024-04-03 15:05:14 +0200

    ProsjektServiceTest.java end of day

--- src/test/java/no/hvl/dat109/prosjekt/ProsjektServiceTest.java
+++ src/test/java/no/hvl/dat109/prosjekt/ProsjektServiceTest.java
@@ -1,16 +1,61 @@
 package no.hvl.dat109.prosjekt;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import no.hvl.dat109.prosjekt.repo.ProsjektRepo;
+import no.hvl.dat109.prosjekt.service.ProsjektService;
+import no.hvl.dat109.prosjekt.entity.Prosjekt;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 class ProsjektServiceTest {
-	
-	//TODO
+
+	private ProsjektService prosjektService;
+	private ProsjektRepo prosjektRepo;
+
+	@BeforeEach
+	public void setup() {
+		prosjektRepo = mock(ProsjektRepo.class);
+		prosjektService = new ProsjektService(prosjektRepo);
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void finnAlleTest() {
+		List<Prosjekt> prosjekter = new ArrayList<>();
+		Prosjekt prosjekt1 = new Prosjekt();
+		prosjekt1.setNavn("1100 Adm");
+		prosjekter.add(prosjekt1);
+
+		Prosjekt prosjekt2 = new Prosjekt();
+		prosjekt2.setNavn("1200 Salg");
+		prosjekter.add(prosjekt2);
+
+		ProsjektService mockProsjektService = mock(ProsjektService.class);
+		when(mockProsjektService.finnAlle()).thenReturn(prosjekter);
+
+		List<Prosjekt> testResultatet = mockProsjektService.finnAlle();
+		assertEquals(prosjekter, testResultatet);
+		assertEquals(2, testResultatet.size());
+	}
+
+	@Test
+	public void finnMedIdTest() {
+		Prosjekt prosjekt = new Prosjekt();
+		prosjekt.setNavn("1400 HR");
+		System.out.println(prosjekt.getId());
+
+		when(prosjektRepo.findById(1)).thenReturn(Optional.of(prosjekt));
+
+		Prosjekt testResultat = prosjektService.finnMedID(1);
+		assertEquals(prosjekt, testResultat);
+		assertEquals(1, testResultat.getId());
+		assertEquals("1400 HR", testResultat.getNavn());
 	}
 
 }
