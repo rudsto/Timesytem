ba4e996ec5ebca2de0e15867ea9a46f03bf13ae9
669802 <111427312+669802@users.noreply.github.com>
2024-04-03 12:29:52 +0200

    ProsjektService.java finnAlle

--- src/main/java/no/hvl/dat109/prosjekt/service/ProsjektService.java
+++ src/main/java/no/hvl/dat109/prosjekt/service/ProsjektService.java
@@ -1,16 +1,18 @@
 package no.hvl.dat109.prosjekt.service;
 
 import no.hvl.dat109.prosjekt.entity.Prosjekt;
+import no.hvl.dat109.prosjekt.repo.ProsjektRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProsjektService {
+
+    private ProsjektRepo prosjektRepo;
 	
     public List<Prosjekt> finnAlle() {
-        //TODO
-        return null;
+        return prosjektRepo.findAll();
     }
 
     public Prosjekt finnMedID(Integer id) {
