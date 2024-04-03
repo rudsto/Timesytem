fe879b4df12e147e3a3075d0ea3877781a367d80
Severin <669799@stud.hvl.no>
2024-04-03 12:17:06 +0200

    14543

--- src/main/java/no/hvl/dat109/prosjekt/service/BrukerService.java
+++ src/main/java/no/hvl/dat109/prosjekt/service/BrukerService.java
@@ -2,28 +2,45 @@ package no.hvl.dat109.prosjekt.service;
 
 import java.util.List;
 
+import no.hvl.dat109.prosjekt.repo.BrukerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import no.hvl.dat109.prosjekt.entity.Bruker;
 
 @Service
 public class BrukerService {
-	
-	//TODO
-	
-	public List<Bruker> finnAlle() {
-		//TODO
-		return null;
-	}
-	
-	public Bruker finnMedMobil(String mobil) {
-		//TODO
-		return null;
-	}
-	
-	public Bruker lagre(Bruker bruker) {
-		//TODO
-		return null;
-	}
+
+    @Autowired
+    BrukerRepo brukerRepo;
+
+    //TODO
+
+    public List<Bruker> finnAlle() {
+        //TODO
+        return null;
+    }
+
+    public Bruker finnMedMobil(String mobil) {
+        //TODO
+        return null;
+    }
+
+    public Bruker lagre(Bruker bruker) {
+        //TODO
+        return null;
+    }
+
+    public List<Bruker> finnAlleBrukere() {
+        return brukerRepo.findAll();
+    }
+
+    public Bruker finnBrukerMedMobil(String mobil) {
+        return brukerRepo.findByMobil(mobil);
+    }
+
+    public Bruker lagreBruker(Bruker bruker) {
+        return brukerRepo.save(bruker);
+    }
 
 }
