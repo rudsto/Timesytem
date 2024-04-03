6112dc755ead9c850d87948234c3e1e50b09c275
669802 <111427312+669802@users.noreply.github.com>
2024-04-03 12:17:34 +0200

    ProsjektService.java lagt til tomme metoder fra Klassemodell og TODO

--- src/main/java/no/hvl/dat109/prosjekt/service/ProsjektService.java
+++ src/main/java/no/hvl/dat109/prosjekt/service/ProsjektService.java
@@ -1,10 +1,28 @@
 package no.hvl.dat109.prosjekt.service;
 
+import no.hvl.dat109.prosjekt.entity.Prosjekt;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProsjektService {
 	
-	//TODO
+    public List<Prosjekt> finnAlle() {
+        //TODO
+        return null;
+    }
+
+    public Prosjekt finnMedID(Integer id) {
+        //TODO
+
+        return null;
+    }
+
+    public Prosjekt lagre(Prosjekt prosjekt) {
+        //TODO
+
+        return null;
+    }
 
 }
