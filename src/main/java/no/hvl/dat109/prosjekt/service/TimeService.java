753d627e8937d2eb0d5dc26dd4b1adddd7c63001
599061 <dag__aa@hotmail.com>
2024-04-04 16:24:07 +0200

    TimeService og TimeServiceTest

--- src/main/java/no/hvl/dat109/prosjekt/service/TimeService.java
+++ src/main/java/no/hvl/dat109/prosjekt/service/TimeService.java
@@ -1,10 +1,31 @@
 package no.hvl.dat109.prosjekt.service;
 
+import no.hvl.dat109.prosjekt.entity.Bruker;
+import no.hvl.dat109.prosjekt.entity.Prosjekt;
+import no.hvl.dat109.prosjekt.entity.Time;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TimeService {
 	
-	//TODO
+	public void finnAlle(){
+
+    }
+
+    public void finnBrukerTimer(Bruker bruker) {
+
+    }
+
+    public void finnProsjektTimer(Prosjekt prosjekt) {
+
+    }
+
+    public void finnBrukerProsjektTimer(Bruker bruker, Prosjekt prosjekt) {
+
+    }
+
+    public void lagre(Time time) {
+
+    }
 
 }
--- src/test/java/no/hvl/dat109/prosjekt/TimeServiceTest.java
+++ src/test/java/no/hvl/dat109/prosjekt/TimeServiceTest.java
@@ -2,14 +2,40 @@ package no.hvl.dat109.prosjekt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import no.hvl.dat109.prosjekt.entity.Bruker;
+import no.hvl.dat109.prosjekt.entity.Prosjekt;
+import no.hvl.dat109.prosjekt.entity.Time;
 import org.junit.jupiter.api.Test;
 
 class TimeServiceTest {
-	
-	//TODO
 
 	@Test
-	void test() {
+	public void finnAlle(){
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void finnBrukerTimer() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void finnProsjektTimer() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void finnBrukerProsjektTimer() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void lagre() {
+
 		fail("Not yet implemented");
 	}
 
