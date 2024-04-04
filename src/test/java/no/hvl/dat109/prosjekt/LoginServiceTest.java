dea7474ce6fc681d142a24048bcbc360f1e43ada
599061 <dag__aa@hotmail.com>
2024-04-04 13:09:45 +0200

    LoginServiceTest

--- src/test/java/no/hvl/dat109/prosjekt/LoginServiceTest.java
+++ src/test/java/no/hvl/dat109/prosjekt/LoginServiceTest.java
@@ -1,70 +1,81 @@
-//package no.hvl.dat109.prosjekt;
+package no.hvl.dat109.prosjekt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import no.hvl.dat109.prosjekt.entity.Bruker;
+import no.hvl.dat109.prosjekt.service.LoginService;
+import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Testklasse for LoginService
+ */
+@ExtendWith(MockitoExtension.class)
+public class LoginServiceTest {
+
+
+    private HttpServletRequest request;
+    private HttpSession session;
+    private Bruker testBruker;
+
+    /**
+     * Initialiserer mock-objekter for videre bruk i test-klassen.
+     */
+    @BeforeEach
+    public void setUp() {
+        request = new MockHttpServletRequest();
+        session = new MockHttpSession();
+        testBruker = new Bruker();
+    }
+
+    /**
+     * Tester om loginBruker logger inn bruker.
+     */
+    @Test
+    public void loggInnBrukerTest() {
+
+        LoginService.loggInnBruker(request, testBruker);
+        session = request.getSession();
+        assertNotNull(session);
+        assertTrue(LoginService.erBrukerInnlogget(session));
+        assertEquals(LoginService.MAX_INACTIVE_INTERVAL, session.getMaxInactiveInterval());
+        assertEquals(testBruker, session.getAttribute("bruker"));
+
+    }
+
+    /**
+     * Tester om loggUtBruker logger ut bruker.
+     */
+    @Test
+    public void loggUtBrukerTest() {
+
+//        LoginService.loggInnBruker(request, testBruker);
+//        session = request.getSession();
+//        LoginService.loggUtBruker(session);
 //
-//import no.hvl.dat109.prosjekt.repo.BrukerRepo;
-//import no.hvl.dat109.prosjekt.service.LoginService;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.mockito.Mockito.*;
-//
-///**
-// * Testklasse for LoginService
-// */
-//@ExtendWith(MockitoExtension.class)
-//public class LoginServiceTest {
-//
-//    @Mock
-//    private static BrukerRepo brukerRepository;
-//
-//    @InjectMocks
-//    private static LoginService loginService;
-//
-//    /**
-//     * Tester om loginBruker logger inn bruker.
-//     */
-//    @Test
-//    public void loggInnBrukerTest() {
-//        String brukernavn = "testBruker";
-//        String passord = "testPassord";
-//
-//        when(brukerRepository.authenticate(brukernavn, passord)).thenReturn(true);
-//
-//        boolean loggetInn = loginService.loggInnBruker(brukernavn, passord);
-//
-//        assertTrue(loggetInn);
-//        verify(brukerRepository, times(1)).authenticate(brukernavn, passord);
-//    }
-//
-//    /**
-//     * Tester om loggUtBruker logger ut bruker.
-//     */
-//    @Test
-//    public void loggUtBrukerTest() {
-//        String brukernavn = "testBruker";
-//
-//        loginService.loggInnBruker(brukernavn, "passord");
-//
-//        loginService.loggUtBruker(brukernavn);
-//
-//        assertFalse(loginService.erBrukerInnlogget(brukernavn));
-//    }
-//
-//    /**
-//     * Tester om erBrukerInnlogget returnerer riktig boolean.
-//     */
-//    @Test
-//    public void erBrukerInnloggetTest() {
-//        String innloggetBruker = "testBruker1";
-//        String utloggetBruker = "testBruker2";
-//
-//        loginService.loggInnBruker(innloggetBruker, "passord");
-//
-//        assertTrue(loginService.erBrukerInnlogget(innloggetBruker));
-//        assertFalse(loginService.erBrukerInnlogget(utloggetBruker));
-//    }
-//}
+//        assertNull(session.getAttribute("bruker"));
+//        assertFalse(LoginService.erBrukerInnlogget(session));
+
+    }
+
+    /**
+     * Tester om erBrukerInnlogget returnerer riktig boolean.
+     */
+    @Test
+    public void erBrukerInnloggetTest() {
+
+//        assertFalse(LoginService.erBrukerInnlogget(session));
+//        LoginService.loggInnBruker(request, testBruker);
+//        assertTrue(LoginService.erBrukerInnlogget(session));
+//        LoginService.loggUtBruker(session);
+//        assertFalse(LoginService.erBrukerInnlogget(session));
+
+    }
+}
