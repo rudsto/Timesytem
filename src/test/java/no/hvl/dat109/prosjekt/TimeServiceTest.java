e7b64aae9194ea3ea8fa62b812cf02898525779f
599061 <dag__aa@hotmail.com>
2024-04-06 15:07:23 +0200

    TimeServiceTest, brother!

--- src/test/java/no/hvl/dat109/prosjekt/TimeServiceTest.java
+++ src/test/java/no/hvl/dat109/prosjekt/TimeServiceTest.java
@@ -1,39 +1,120 @@
 package no.hvl.dat109.prosjekt;
 
+import no.hvl.dat109.prosjekt.entity.Bruker;
+import no.hvl.dat109.prosjekt.entity.Prosjekt;
+import no.hvl.dat109.prosjekt.entity.Time;
+import no.hvl.dat109.prosjekt.repo.TimeRepo;
+import no.hvl.dat109.prosjekt.service.TimeService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TimeServiceTest {
 
-	@Test
-	public void finnAlle(){
+	@Mock
+	TimeRepo timeRepo;
 
-		fail("Not yet implemented");
+	@InjectMocks
+	TimeService timeService;
+
+
+	@Test
+	public void finnAlleTimerTest(){
+
+		Time time1 = new Time();
+		Time time2 = new Time();
+		List<Time> timer = Arrays.asList(time1, time2);
+
+		when(timeRepo.findAll()).thenReturn(timer);
+
+		List<Time> resultat = timeService.finnAlleTimer();
+
+		assertEquals(timer, resultat);
+		assertEquals(2, resultat.size());
 	}
 
 	@Test
-	public void finnBrukerTimer() {
+	public void finnBrukerTimerTest() {
 
-		fail("Not yet implemented");
+		Bruker bruker = new Bruker();
+		Time time1 = new Time();
+		Time time2 = new Time();
+		time1.setBruker(bruker);
+		time2.setBruker(bruker);
+		List<Time> timer = Arrays.asList(time1, time2);
+
+		when(timeRepo.findAll()).thenReturn(timer);
+
+		List<Time> resultat = timeService.finnBrukerTimer(bruker);
+
+		assertEquals(bruker, time1.getBruker());
+		assertEquals(bruker, time2.getBruker());
+		assertEquals(2, resultat.size());
 	}
 
 	@Test
-	public void finnProsjektTimer() {
+	public void finnProsjektTimerTest() {
 
-		fail("Not yet implemented");
+		Prosjekt prosjekt = new Prosjekt();
+		Time time1 = new Time();
+		Time time2 = new Time();
+		time1.setProsjekt(prosjekt);
+		time2.setProsjekt(prosjekt);
+		List<Time> timer = Arrays.asList(time1, time2);
+
+		when(timeRepo.findAll()).thenReturn(timer);
+
+		List<Time> resultat = timeService.finnProsjektTimer(prosjekt);
+
+		assertEquals(prosjekt, time1.getProsjekt());
+		assertEquals(prosjekt, time2.getProsjekt());
+		assertEquals(2, resultat.size());
 	}
 
 	@Test
-	public void finnBrukerProsjektTimer() {
+	public void finnBrukerProsjektTimerTest() {
 
-		fail("Not yet implemented");
+		Bruker bruker = new Bruker();
+		Prosjekt prosjekt = new Prosjekt();
+		Time time1 = new Time();
+		Time time2 = new Time();
+		time1.setBruker(bruker);
+		time2.setBruker(bruker);
+		time1.setProsjekt(prosjekt);
+		time2.setProsjekt(prosjekt);
+		List<Time> timer = Arrays.asList(time1, time2);
+
+		when(timeRepo.findAll()).thenReturn(timer);
+
+		List<Time> resultat = timeService.finnBrukerProsjektTimer(bruker, prosjekt);
+
+		assertEquals(bruker, time1.getBruker());
+		assertEquals(bruker, time2.getBruker());
+		assertEquals(prosjekt, time1.getProsjekt());
+		assertEquals(prosjekt, time2.getProsjekt());
+		assertEquals(2, resultat.size());
 	}
 
 	@Test
-	public void lagre() {
+	public void lagreTimeTest() {
 
-		fail("Not yet implemented");
+		Time time = new Time();
+		time.setAntallTimer(8);
+
+		when(timeService.lagreTime(time)).thenReturn(time);
+
+		Time resultat = timeService.lagreTime(time);
+		assertEquals(time, resultat);
+		assertEquals(8, resultat.getAntallTimer());
 	}
 
 }
