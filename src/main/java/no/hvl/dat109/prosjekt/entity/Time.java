a5faa2c665118e93ebb1fe14ad8ad424b707a1c0
Severin <669799@stud.hvl.no>
2024-04-06 14:47:33 +0200

    Time entity

--- src/main/java/no/hvl/dat109/prosjekt/entity/Time.java
+++ src/main/java/no/hvl/dat109/prosjekt/entity/Time.java
@@ -1,11 +1,12 @@
 package no.hvl.dat109.prosjekt.entity;
 
 import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
 public class Time {
-	
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -19,4 +20,42 @@ public class Time {
     @JoinColumn(name = "prosjekt_id")
     private Prosjekt prosjekt;
 
+    public Time() {
+
+    }
+
+    public Time(int antallTimer, Bruker bruker, Prosjekt prosjekt) {
+        this.antallTimer = antallTimer;
+        this.bruker = bruker;
+        this.prosjekt = prosjekt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public int getAntallTimer() {
+        return antallTimer;
+    }
+
+    public void setAntallTimer(int antallTimer) {
+        this.antallTimer = antallTimer;
+    }
+
+    public Bruker getBruker() {
+        return bruker;
+    }
+
+    public void setBruker(Bruker bruker) {
+        this.bruker = bruker;
+    }
+
+    public Prosjekt getProsjekt() {
+        return prosjekt;
+    }
+
+    public void setProsjekt(Prosjekt prosjekt) {
+        this.prosjekt = prosjekt;
+    }
 }
