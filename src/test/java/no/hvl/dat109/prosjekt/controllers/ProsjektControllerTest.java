8bc7db3b1fbd2da0cfdd6a88dfb4065b5a1acb8c
669802 <111427312+669802@users.noreply.github.com>
2024-04-08 21:28:11 +0200

    Oppsett ProsjektControllerTest.java

--- src/test/java/no/hvl/dat109/prosjekt/controllers/ProsjektControllerTest.java
+++ src/test/java/no/hvl/dat109/prosjekt/controllers/ProsjektControllerTest.java
@@ -1,4 +1,28 @@
 package no.hvl.dat109.prosjekt.controllers;
 
+import no.hvl.dat109.prosjekt.service.ProsjektService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+
+@SpringBootTest(classes = ProsjektController.class)
 public class ProsjektControllerTest {
+
+    @Autowired
+    private ProsjektController prosjektController;
+
+    @MockBean
+    private ProsjektService prosjektService;
+
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    //TODO
+
+
 }
