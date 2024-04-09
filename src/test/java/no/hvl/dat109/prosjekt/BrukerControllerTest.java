9b92525229c65d324efdb98d105667f5eddc35c2
599061 <dag__aa@hotmail.com>
2024-04-09 12:45:51 +0200

    BrukerControllerTest finally!!!

--- src/test/java/no/hvl/dat109/prosjekt/BrukerControllerTest.java
+++ src/test/java/no/hvl/dat109/prosjekt/BrukerControllerTest.java
@@ -9,7 +9,6 @@ import no.hvl.dat109.prosjekt.service.ProsjektService;
 import no.hvl.dat109.prosjekt.service.TimeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
