02e47b4e3c74d7537d93d8595bd9f9c68a03ebda
669802 <111427312+669802@users.noreply.github.com>
2024-04-10 13:11:14 +0200

    Lagt til javadoc på getMapping i SlettProsjektController.java

--- src/main/java/no/hvl/dat109/prosjekt/controllers/SlettProsjektController.java
+++ src/main/java/no/hvl/dat109/prosjekt/controllers/SlettProsjektController.java
@@ -17,10 +17,20 @@ public class SlettProsjektController {
     @Autowired
     private ProsjektService prosjektService;
 
+    /**
+     * Viser skjemaet for sletting av prosjekt.
+     * Metoden sjekker først at brukeren er innlogget ved sjekk av HttpSession.
+     * Hvis brukeren ikke er logget inn, blir brukeren omdirigert til innloggingen med feilmelding.
+     *
+     * @param session   Den nåværende {@link HttpSession} som brukes for å verifisere om brukeren er innlogget
+     * @param model     {@link Model} objekt som brukes for å sende data til visningen
+     * @param ra        {@link RedirectAttributes} som brukes for å sende feilmeldinger over til redirect
+     * @return          {@link String} som representerer navnet på visningen som skal hentes.
+     */
     @GetMapping("slettprosjekt")
     public String getSlettProsjekt(HttpSession session, Model model, RedirectAttributes ra) {
         if(!LoginUtil.erBrukerInnlogget(session)) {
-            ra.addFlashAttribute("feilmelding", "Du må være innlogget for å opprette prosjekt");
+            ra.addFlashAttribute("feilmelding", "Du må være innlogget for å slette prosjekt");
             return "redirect:login";
         }
 
