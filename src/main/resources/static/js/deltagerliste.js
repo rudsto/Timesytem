8bc4539beafb7e4708543d22c20897a465a82b3c
leo <heldal.leonard@gmail.com>
2024-04-11 15:00:28 +0200

    evenly spaced tabs

--- src/main/resources/static/js/deltagerliste.js
+++ src/main/resources/static/js/deltagerliste.js
@@ -1,7 +1,27 @@
 "use strict";
 
 function expandView(evt, tabName){
-    // Declare all variables
+    // Variable for the current tab's content
+    let tabcontent;
+
+    // Get the element of the current tab content
+    tabcontent = document.getElementById(tabName);
+
+    // If the current tab is already displayed, hide it and remove "active" class
+    if (tabcontent.style.display === "block") {
+        tabcontent.style.display = "none";
+        evt.currentTarget.classList.remove("active");
+    } else {
+        // Hide all other tabs
+        hideAllTabs();
+
+        // Show the current tab and add "active" class
+        tabcontent.style.display = "block";
+        evt.currentTarget.classList.add("active");
+    }
+}
+
+function hideAllTabs(){
     let i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
@@ -13,10 +33,7 @@ function expandView(evt, tabName){
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].classList.remove("active");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
 }
