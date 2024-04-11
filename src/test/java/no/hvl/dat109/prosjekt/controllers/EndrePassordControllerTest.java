ca5db5c81559f9635a5bcf8ebd2af77b61f3e765
Aleksander <spjort2003@gmail.com>
2024-04-11 19:35:13 +0200

    nye endre passord tester

--- src/test/java/no/hvl/dat109/prosjekt/controllers/EndrePassordControllerTest.java
+++ src/test/java/no/hvl/dat109/prosjekt/controllers/EndrePassordControllerTest.java
@@ -1,11 +1,21 @@
 package no.hvl.dat109.prosjekt.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import javax.servlet.http.HttpServletRequest;
+
 import no.hvl.dat109.prosjekt.Utils.LoginUtil;
 import no.hvl.dat109.prosjekt.Utils.PassordUtil;
 import no.hvl.dat109.prosjekt.entity.Bruker;
 import no.hvl.dat109.prosjekt.service.BrukerService;
-import no.hvl.dat109.prosjekt.service.ProsjektService;
-import no.hvl.dat109.prosjekt.service.TimeService;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,12 +29,6 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import javax.servlet.http.HttpServletRequest;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 /**
  * Testklasse for BrukerController.
  */
@@ -37,10 +41,6 @@ public class EndrePassordControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private BrukerService brukerService;
-    @MockBean
-    private ProsjektService prosjektService;
-    @MockBean
-    private TimeService timeService;
     @InjectMocks
     private BrukerController brukerController;
 
@@ -58,6 +58,7 @@ public class EndrePassordControllerTest {
         Bruker testBruker = new Bruker();
 
         LoginUtil.loggInnBruker(request, testBruker);
+        session.setAttribute("bruker", testBruker);
 
         mockMvc.perform(get("/endrepassord").session(session))
                 .andExpect(status().isOk())
@@ -83,7 +84,7 @@ public class EndrePassordControllerTest {
      */
     @Test
     public void endrePassordSuksessTest() throws Exception {
-        MockHttpSession session = new MockHttpSession();
+    	MockHttpSession session = new MockHttpSession();
         HttpServletRequest request = new MockHttpServletRequest();
         Bruker testBruker = new Bruker();
         
@@ -91,14 +92,107 @@ public class EndrePassordControllerTest {
         testBruker.setHash(PassordUtil.hashMedSalt("passs", testBruker.getSalt()));
 
         LoginUtil.loggInnBruker(request, testBruker);
+        session.setAttribute("bruker", testBruker);
 
         mockMvc.perform(post("/endrepassord")
+        		.session(session)
         		.param("gammeltpassord", "passs")
         		.param("nyttpassord", "12345")
                 .param("gjentattnyttPassord", "12345"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("endrepassord_suksess"))
                 .andExpect(flash().attributeExists("bruker"));
+    }
+    
+    /**
+     * Tester om hash og salt blir endret i bruker ved gyldig input.
+     * @throws Exception dersom det kommer feilmelding ved testkjøring
+     */
+    @Test
+    public void passordBlirEndretTest() throws Exception {
+    	MockHttpSession session = new MockHttpSession();
+        HttpServletRequest request = new MockHttpServletRequest();
+        Bruker testBruker = new Bruker();
+        
+        String tidligerePassord = "passs";
+        String tidligereSalt = PassordUtil.genererTilfeldigSalt();
+        String tidligereHash = PassordUtil.hashMedSalt(tidligerePassord, tidligereSalt);
+        String nyttPassord = "12345";
+        
+        testBruker.setSalt(tidligereSalt);
+        testBruker.setHash(tidligereHash);
+
+        LoginUtil.loggInnBruker(request, testBruker);
+        session.setAttribute("bruker", testBruker);
+    	
+        mockMvc.perform(post("/endrepassord")
+        		.session(session)
+        		.param("gammeltpassord", tidligerePassord)
+        		.param("nyttpassord", nyttPassord)
+                .param("gjentattnyttPassord", nyttPassord));
+        
+        assertFalse(testBruker.getSalt().equals(tidligereSalt));
+        assertFalse(testBruker.getHash().equals(tidligereHash));
+        assertTrue(PassordUtil.erKorrektPassord(nyttPassord, testBruker.getSalt(), testBruker.getHash()));
+    }
+    
+    /**
+     * Tester om hash og salt ikke blir endret i bruker ved ugyldig input.
+     * @throws Exception dersom det kommer feilmelding ved testkjøring
+     */
+    @Test
+    public void passordBlirIkkeEndretTest() throws Exception {
+    	MockHttpSession session = new MockHttpSession();
+        HttpServletRequest request = new MockHttpServletRequest();
+        Bruker testBruker = new Bruker();
+        
+        String tidligerePassord = "passs";
+        String tidligereSalt = PassordUtil.genererTilfeldigSalt();
+        String tidligereHash = PassordUtil.hashMedSalt(tidligerePassord, tidligereSalt);
+        
+        testBruker.setSalt(tidligereSalt);
+        testBruker.setHash(tidligereHash);
+
+        LoginUtil.loggInnBruker(request, testBruker);
+
+        // Test at det ikke blir endret ved for kort nytt passord
+        mockMvc.perform(post("/endrepassord")
+        		.session(session)
+        		.param("gammeltpassord", tidligerePassord)
+        		.param("nyttpassord", "123")
+                .param("gjentattnyttPassord", "123"));
+        
+        assertTrue(testBruker.getSalt().equals(tidligereSalt));
+        assertTrue(testBruker.getHash().equals(tidligereHash));
+        assertTrue(PassordUtil.erKorrektPassord(tidligerePassord, testBruker.getSalt(), testBruker.getHash()));
+        
+        testBruker.setSalt(tidligereSalt);
+        testBruker.setHash(tidligereHash);
+        
+        // Test at det ikke blir endret ved feil tidligere passord
+        mockMvc.perform(post("/endrepassord")
+        		.session(session)
+        		.param("gammeltpassord", "peaead")
+        		.param("nyttpassord", "12345")
+                .param("gjentattnyttPassord", "12345"));
+        
+        assertTrue(testBruker.getSalt().equals(tidligereSalt));
+        assertTrue(testBruker.getHash().equals(tidligereHash));
+        assertTrue(PassordUtil.erKorrektPassord(tidligerePassord, testBruker.getSalt(), testBruker.getHash()));
+        
+        testBruker.setSalt(tidligereSalt);
+        testBruker.setHash(tidligereHash);
+        
+        // Test at det ikke blir endret ved ulikt gjentatt nytt passord
+        mockMvc.perform(post("/endrepassord")
+        		.session(session)
+        		.param("gammeltpassord", tidligerePassord)
+        		.param("nyttpassord", "12345")
+                .param("gjentattnyttPassord", "67890"));
+        
+        assertTrue(testBruker.getSalt().equals(tidligereSalt));
+        assertTrue(testBruker.getHash().equals(tidligereHash));
+        assertTrue(PassordUtil.erKorrektPassord(tidligerePassord, testBruker.getSalt(), testBruker.getHash()));
     }
     
     /**
@@ -115,8 +209,10 @@ public class EndrePassordControllerTest {
         testBruker.setHash(PassordUtil.hashMedSalt("passs", testBruker.getSalt()));
 
         LoginUtil.loggInnBruker(request, testBruker);
+        session.setAttribute("bruker", testBruker);
 
         mockMvc.perform(post("/endrepassord")
+        		.session(session)
         		.param("gammeltpassord", "12345")
         		.param("nyttpassord", "125")
                 .param("gjentattnyttPassord", "14345"))
@@ -125,4 +221,6 @@ public class EndrePassordControllerTest {
                 .andExpect(flash().attributeExists("endrePassordFeilmelding"));
     }
 }
+
+
 
